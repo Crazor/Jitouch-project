b3ad40782509fa4a3cf8d3c890ba70a9e756bff5
Aaron Kollasch <aaron@kollasch.dev>
2022-07-05 14:41:27 -0400

    Improve spacing in key debug message

--- Application/Gesture.m
+++ Application/Gesture.m
@@ -887,13 +887,13 @@ static void doCommand(NSString *gesture, int device) {
                 tmpRef = activateWindowAtPosition(x, y);
             
             NSUInteger modifierFlags = [[commandDict objectForKey:@"ModifierFlags"] unsignedIntegerValue];
-            if (logLevel >= LOG_LEVEL_DEBUG) NSLog(@"Key \"%@%@%@%@%@\" for application \"%@\"",
-                                                   (modifierFlags & kCGEventFlagMaskShift)? @"⇧" : @"",
-                                                   (modifierFlags & kCGEventFlagMaskControl)? @"⌃" : @"",
-                                                   (modifierFlags & kCGEventFlagMaskAlternate)? @"⌥" : @"",
-                                                   (modifierFlags & kCGEventFlagMaskCommand)? @"⌘" : @"",
-                                                   [KeyUtility codeToChar:(CGKeyCode)[[commandDict objectForKey:@"KeyCode"] unsignedIntValue]],
-                                                   application);
+            DDLogDebug(@"Key \"%@%@%@%@%@\" for application \"%@\"",
+                       (modifierFlags & kCGEventFlagMaskShift)? @"⇧" : @"",
+                       (modifierFlags & kCGEventFlagMaskControl)? @"⌃" : @"",
+                       (modifierFlags & kCGEventFlagMaskAlternate)? @"⌥ " : @"",
+                       (modifierFlags & kCGEventFlagMaskCommand)? @"⌘ " : @"",
+                       [KeyUtility codeToChar:(CGKeyCode)[[commandDict objectForKey:@"KeyCode"] unsignedIntValue]],
+                       application);
             [keyUtil simulateKeyCode:[[commandDict objectForKey:@"KeyCode"] unsignedShortValue]
                             ShftDown:(modifierFlags & kCGEventFlagMaskShift) != 0
                             CtrlDown:(modifierFlags & kCGEventFlagMaskControl) != 0
